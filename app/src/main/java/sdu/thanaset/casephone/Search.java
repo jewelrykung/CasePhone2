b9570243d34b59c0b2a82cbeceadbbcfdcbae7aa
Thanaset <jewelry_kung@hotmail.com>
2017-04-27 21:32:03 +0700

    4 page ใกล้เสร็จ

--- app/src/main/java/sdu/thanaset/casephone/Search.java
+++ app/src/main/java/sdu/thanaset/casephone/Search.java
@@ -5,21 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Home extends AppCompatActivity {
+public class Search extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-
-    }
-    public void searchbnt (View view){
-        Intent intent = new Intent(Home.this,Search.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_search);
     }
     public void homebnt (View view){
-        Intent intent = new Intent(Home.this,Home.class);
+        Intent intent = new Intent(Search.this,Home.class);
         startActivity(intent);
     }
 }
