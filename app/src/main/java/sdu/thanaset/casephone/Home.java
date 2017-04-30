25ee17e4e1b54eca9ef98fc867574214482ae926
Thanaset <jewelry_kung@hotmail.com>
2017-04-30 12:30:31 +0700

    แอดข้อมูลเข้า listview

--- app/src/main/java/sdu/thanaset/casephone/Home.java
+++ app/src/main/java/sdu/thanaset/casephone/Home.java
@@ -4,39 +4,63 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class Home extends AppCompatActivity {
 
+    ListView listView;
+    private int[] ints =new int[]{R.drawable.huaweiaghjk,R.drawable.huaweiahgjk,R.drawable.huaweiahjkgl,R.drawable.huaweiajjf,
+                                  R.drawable.huaweiasssdg,R.drawable.huaweiassss,R.drawable.huaweiassss,R.drawable.huaweiasssdg,
+                                  R.drawable.huaweiajjf,R.drawable.huaweiahjkgl,R.drawable.huaweiahgjk,R.drawable.huaweiatryu,
+                                  R.drawable.huaweiauti,R.drawable.huaweiauyi,R.drawable.huaweiazxcxvbvjm,R.drawable.huaweiawer,};
+
+    private String[] titleStrings, detailStrings,shortStrings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //Initial view
+        listView = (ListView) findViewById(R.id.liveTraffic);
+        //get value
+        titleStrings = getResources().getStringArray(R.array.title);
+        detailStrings = getResources().getStringArray(R.array.detail);
+
+        //Substring detailstring ตัดคำในส่วนของ detailstring เพื่อไม่มีข้อความไม่เกิน30 char
+        // shortStrings = new String[detailStrings.length];//จอง พื้นที่ในหน่วยความจำ shortstring
+        //for (int i=0;i<detailStrings.length;i++){
+        //    shortStrings[i]=detailStrings[i].substring(0,29)+"...";
+        // }//end for
+        //create listview
+        MyAdapter myAdapter = new MyAdapter(Home.this,ints,titleStrings,detailStrings);
+        listView.setAdapter(myAdapter);
+
+    }
 
 
-    }
-    public void searchbnt (View view){
-        Intent intent = new Intent(Home.this,Search.class);
-        startActivity(intent);
-    }
-    public void homebnt (View view){
-        Intent intent = new Intent(Home.this,Home.class);
-        startActivity(intent);
-    }
-    public void highlightbtn (View view){
-        Intent intent = new Intent(Home.this,Highlight.class);
-        startActivity(intent);
-    }
-    public void basketbtn (View view){
-        Intent intent = new Intent(Home.this,Basket.class);
-        startActivity(intent);
-    }
-    public void paybtn (View view){
-        Intent intent = new Intent(Home.this,Pay.class);
-        startActivity(intent);
-    }
+   // public void searchbnt (View view){
+     //   Intent intent = new Intent(Home.this,Search.class);
+    //    startActivity(intent);
+   // }
+   // public void homebnt (View view){
+    //    Intent intent = new Intent(Home.this,Home.class);
+    //    startActivity(intent);
+    //}
+   // public void highlightbtn (View view){
+    //    Intent intent = new Intent(Home.this,Highlight.class);
+   //     startActivity(intent);
+    //}
+   // public void basketbtn (View view){
+   //     Intent intent = new Intent(Home.this,Basket.class);
+    //    startActivity(intent);
+   // }
+   // public void paybtn (View view){
+   //     Intent intent = new Intent(Home.this,Pay.class);
+   //     startActivity(intent);
+   // }
 
-    public void btnloginhome (View view){
-        Intent intent = new Intent(Home.this,MainActivity.class);
-        startActivity(intent);
-    }
+  //  public void btnloginhome (View view){
+   //     Intent intent = new Intent(Home.this,MainActivity.class);
+   //     startActivity(intent);
+   // }
 }
---
+++ app/src/main/java/sdu/thanaset/casephone/MyAdapter.java
@@ -0,0 +1,61 @@
+package sdu.thanaset.casephone;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Thanaset on 30/4/2560.
+ */
+
+public class MyAdapter extends BaseAdapter {
+
+    //Explicit
+    private Context context;
+    private int [] ints;
+    private String [] titleStrings,detailStrings;
+
+    public MyAdapter(Context context, int[] ints, String[] titleStrings, String[] detailStrings) {
+        this.context = context;
+        this.ints = ints;
+        this.titleStrings = titleStrings;
+        this.detailStrings = detailStrings;
+    }
+
+    @Override
+    public int getCount( ) { //ใช้นับจำนวนข้อมูล แล้วส่งไป getview
+        return ints.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {//รับจำนวนข้อมูลแล้วนำไปแสดงผลต่อบนหน้าเว็ป
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view =layoutInflater.inflate(R.layout.phone,parent,false);//ของmylistview
+        //intitial view ผูกIDของ view กับตัวแปร เพื่อให้รู้จักกัน
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.imvIcon);
+        TextView titletextView = (TextView) view.findViewById(R.id.txtTitleLiv);
+        TextView detailtextView  = (TextView) view.findViewById(R.id.txtDetaiLiv);
+        // show view นำข้อมูลไปแสดงผมบนห้า app
+        imageView.setImageResource(ints[position]);
+        titletextView.setText(titleStrings[position]);
+        detailtextView.setText(detailStrings[position]);
+
+
+        return view;
+    }
+} // main class
